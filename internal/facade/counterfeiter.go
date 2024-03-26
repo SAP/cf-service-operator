9e7ac22753999cd1d0d579565cb306555f1de55e
d039803 <ralf.hammer@sap.com>
2024-03-26 09:14:53 +0100

    add license header

--- internal/facade/counterfeiter.go
+++ internal/facade/counterfeiter.go
@@ -1,3 +1,8 @@
+/*
+SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and cf-service-operator contributors
+SPDX-License-Identifier: Apache-2.0
+*/
+
 package facade
 
 // required once per package that contains interfaces to be mocked
