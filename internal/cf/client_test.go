501162def602c9bdd454e75bf0e79c7c15554789
uwefreidank <49634966+uwefreidank@users.noreply.github.com>
2024-07-02 12:53:26 +0200

    add custom metrics (#49)
    * add http transport metrics
    make webhooks optional
    
    * Delete README-FORK.md
    
    * add license header
    
    * remove linter findings in README
    
    * check for registered metrics on CF clients
    
    * use constants and update comments
    
    * write metrics to file (only for debugging)
    
    * check metrics >= 0
    
    * metrics has to exist
    
    * add webhookCertDir
    
    ---------
    
    Co-authored-by: RalfHammer <119853077+RalfHammer@users.noreply.github.com>

--- internal/cf/client_test.go
+++ internal/cf/client_test.go
@@ -13,6 +13,8 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/ghttp"
+	"github.com/prometheus/client_golang/prometheus"
+	"sigs.k8s.io/controller-runtime/pkg/metrics"
 )
 
 // constants useful for this file
@@ -90,10 +92,11 @@ var _ = Describe("CF Client tests", Ordered, func() {
 
 	Describe("NewOrganizationClient", func() {
 		BeforeEach(func() {
-			// Reset the cache so tests can be run independently
+			// Reset some entities to enable tests to run independently
 			clientCache = make(map[clientIdentifier]*clientCacheEntry)
-			// Reset server call counts
+			metrics.Registry = prometheus.NewRegistry()
 			server.Reset()
+
 			// Register handlers
 			server.RouteToHandler("GET", "/", ghttp.CombineHandlers(
 				ghttp.RespondWithJSONEncodedPtr(&statusCode, &rootResult),
@@ -133,6 +136,19 @@ var _ = Describe("CF Client tests", Ordered, func() {
 			Expect(server.ReceivedRequests()[2].URL.Path).To(Equal(spacesURI))
 
 			Expect(server.ReceivedRequests()).To(HaveLen(3))
+
+			// verify metrics
+			metricsList, err := metrics.Registry.Gather()
+			Expect(err).To(BeNil())
+			Expect(metricsList).To(HaveLen(3))
+			verified := false
+			for _, m := range metricsList {
+				if *m.Name == "http_client_requests_total" {
+					Expect(m.Metric[0].Counter.GetValue()).To(BeNumerically(">", 0))
+					verified = true
+				}
+			}
+			Expect(verified).To(BeTrue())
 		})
 
 		It("should be able to query some org twice", func() {
@@ -190,10 +206,11 @@ var _ = Describe("CF Client tests", Ordered, func() {
 
 	Describe("NewSpaceClient", func() {
 		BeforeEach(func() {
-			// Reset the cache so tests can be run independently
+			// Reset some entities to enable tests to run independently
 			clientCache = make(map[clientIdentifier]*clientCacheEntry)
-			// Reset server call counts
+			metrics.Registry = prometheus.NewRegistry()
 			server.Reset()
+
 			// Register handlers
 			server.RouteToHandler("GET", "/", ghttp.CombineHandlers(
 				ghttp.RespondWithJSONEncodedPtr(&statusCode, &rootResult),
@@ -216,7 +233,7 @@ var _ = Describe("CF Client tests", Ordered, func() {
 			Expect(server.ReceivedRequests()).To(HaveLen(1))
 		})
 
-		It("should be able to query space", func() {
+		It("should be able to query some space", func() {
 			spaceClient, err := NewSpaceClient(OrgName, url, Username, Password)
 			Expect(err).To(BeNil())
 
@@ -233,6 +250,19 @@ var _ = Describe("CF Client tests", Ordered, func() {
 			Expect(server.ReceivedRequests()[2].URL.Path).To(Equal(serviceInstancesURI))
 
 			Expect(server.ReceivedRequests()).To(HaveLen(3))
+
+			// verify metrics
+			metricsList, err := metrics.Registry.Gather()
+			Expect(err).To(BeNil())
+			Expect(metricsList).To(HaveLen(3))
+			verified := false
+			for _, m := range metricsList {
+				if *m.Name == "http_client_requests_total" {
+					Expect(m.Metric[0].Counter.GetValue()).To(BeNumerically(">", 0))
+					verified = true
+				}
+			}
+			Expect(verified).To(BeTrue())
 		})
 
 		It("should be able to query some space twice", func() {
@@ -284,6 +314,47 @@ var _ = Describe("CF Client tests", Ordered, func() {
 			// Get instance
 			Expect(server.ReceivedRequests()[3].Method).To(Equal("GET"))
 			Expect(server.ReceivedRequests()[3].RequestURI).To(ContainSubstring(Owner2))
+		})
+
+		It("should register prometheus metrics for OrgClient", func() {
+			orgClient, err := NewOrganizationClient(OrgName, url, Username, Password)
+			Expect(err).To(BeNil())
+			Expect(orgClient).ToNot(BeNil())
+
+			// retrieve names of registered metrics
+			metricsList, err := metrics.Registry.Gather()
+			Expect(err).To(BeNil())
+			Expect(metricsList).To(HaveLen(3))
+			metricNames := make([]string, len(metricsList))
+			for i, m := range metricsList {
+				metricNames[i] = *m.Name
+			}
+
+			Expect(metricNames).To(ContainElement("http_client_request_duration_seconds"))
+			Expect(metricNames).To(ContainElement("http_client_requests_in_flight"))
+			Expect(metricNames).To(ContainElement("http_client_requests_total"))
+		})
+
+		It("should register prometheus metrics for SpaceClient", func() {
+			spaceClient, err := NewSpaceClient(SpaceName, url, Username, Password)
+			Expect(err).To(BeNil())
+			Expect(spaceClient).ToNot(BeNil())
+
+			// retrieve names of registered metrics
+			metricsList, err := metrics.Registry.Gather()
+			Expect(err).To(BeNil())
+			Expect(metricsList).To(HaveLen(3))
+			metricNames := make([]string, len(metricsList))
+			for i, m := range metricsList {
+				metricNames[i] = *m.Name
+			}
+
+			Expect(metricNames).To(ContainElement("http_client_request_duration_seconds"))
+			Expect(metricNames).To(ContainElement("http_client_requests_in_flight"))
+			Expect(metricNames).To(ContainElement("http_client_requests_total"))
+
+			// for debugging: write metrics to file
+			// prometheus.WriteToTextfile("metrics.txt", metrics.Registry)
 		})
 
 	})
---
+++ pkg/metrics/transport.go
@@ -0,0 +1,132 @@
+/*
+SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and cf-service-operator contributors
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package metrics
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+const (
+	metricsNamespace = ""
+	metricsSubsystem = "http_client"
+)
+
+// IndependentExecutionGeneral runs several operations independently of each other and only propagates the error value
+func IndependentExecutionGeneral(fns ...func() error) error {
+	var combinedError error
+	for _, f := range fns {
+		err := f()
+		if err != nil {
+			if combinedError == nil {
+				combinedError = err
+			} else {
+				// TODO improve error wrapping
+				// nolint:errorlint // we will improve the error wrapping in the future
+				combinedError = fmt.Errorf("%s; %s", combinedError.Error(), err.Error())
+			}
+		}
+	}
+
+	return combinedError
+}
+
+// AddMetricsToTransport injects the Prometheus metrics to the HTTP transport
+func AddMetricsToTransport(transport http.RoundTripper, registry prometheus.Registerer, target string, host string) (http.RoundTripper, error) {
+	if transport == nil {
+		transport = http.DefaultTransport
+	}
+	constLabels := prometheus.Labels{
+		"target": target,
+		"host":   host,
+	}
+
+	// create metrics
+	inFlightGauge := prometheus.NewGauge(prometheus.GaugeOpts{
+		Name:        "requests_in_flight",
+		Subsystem:   metricsSubsystem,
+		Namespace:   metricsNamespace,
+		Help:        "The number in-flight requests for corresponding HTTP client",
+		ConstLabels: constLabels,
+	})
+	counter := prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name:        "requests_total",
+			Subsystem:   metricsSubsystem,
+			Namespace:   metricsNamespace,
+			Help:        "The number of HTTP requests from corresponding HTTP client",
+			ConstLabels: constLabels,
+		},
+		[]string{"code", "method"},
+	)
+	histVec := prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:        "request_duration_seconds",
+			Subsystem:   metricsSubsystem,
+			Namespace:   metricsNamespace,
+			Help:        "A histogram of HTTP request latencies.",
+			Buckets:     prometheus.DefBuckets,
+			ConstLabels: constLabels,
+		},
+		[]string{},
+	)
+
+	// register created metrics or replace them by already registered ones
+	err := IndependentExecutionGeneral(
+		func() error {
+			err := registry.Register(inFlightGauge)
+			if err == nil {
+				return nil
+			}
+			var e prometheus.AlreadyRegisteredError
+			if errors.As(err, &e) {
+				if collector, ok := e.ExistingCollector.(prometheus.Gauge); ok {
+					inFlightGauge = collector
+					return nil
+				}
+			}
+			return err
+		},
+		func() error {
+			err := registry.Register(counter)
+			if err == nil {
+				return nil
+			}
+			var e prometheus.AlreadyRegisteredError
+			if errors.As(err, &e) {
+				if collector, ok := e.ExistingCollector.(*prometheus.CounterVec); ok {
+					counter = collector
+					return nil
+				}
+			}
+			return err
+		},
+		func() error {
+			err := registry.Register(histVec)
+			if err == nil {
+				return nil
+			}
+			var e prometheus.AlreadyRegisteredError
+			if errors.As(err, &e) {
+				if collector, ok := e.ExistingCollector.(*prometheus.HistogramVec); ok {
+					histVec = collector
+					return nil
+				}
+			}
+			return err
+		},
+	)
+
+	transport = promhttp.InstrumentRoundTripperInFlight(inFlightGauge, transport)
+	transport = promhttp.InstrumentRoundTripperCounter(counter, transport)
+	transport = promhttp.InstrumentRoundTripperDuration(histVec, transport)
+
+	return transport, err
+}
